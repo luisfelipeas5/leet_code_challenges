5f32756a0f9fed89c14e0312506902a801214598
Luis Felipe A.S <luisfelipeas5@gmail.com>
2023-08-12 11:51:53 -0300

    Valid parentheses (#1)
    * test: creates tests for valid parentheses
    
    * first valid parentheses solution
    
    * test: adds more tests
    
    * chore: adds leetcode url at the top

---
+++ lib/valid_parentheses/valid_parentheses.dart
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/valid-parentheses
+
+class Solution {
+  bool isValid(String s) {
+    if (s.length % 2 != 0) return false;
+
+    const Map<String, String> closeMap = {
+      ")": "(",
+      "]": "[",
+      "}": "{",
+    };
+
+    final List<String> openList = <String>[];
+
+    for (var charIndex = 0; charIndex < s.length; charIndex++) {
+      final String char = s[charIndex];
+
+      final String? openPair = closeMap[char];
+      final bool isOpenChar = openPair == null;
+
+      if (isOpenChar) {
+        openList.add(char);
+      } else if (openList.isEmpty || openList.last != openPair) {
+        return false;
+      } else {
+        openList.removeLast();
+      }
+    }
+
+    return openList.isEmpty;
+  }
+}
---
+++ test/valid_parentheses/valid_parentheses_test.dart
@@ -0,0 +1,68 @@
+import 'package:leet_code_challenges/valid_parentheses/valid_parentheses.dart';
+import 'package:test/test.dart';
+
+void main() {
+  late Solution solution;
+
+  group("Valid Parentheses", () {
+    setUp(() {
+      solution = Solution();
+    });
+
+    test(
+      "when s is '(', "
+      "then expect to return false",
+      () {
+        expect(solution.isValid("("), isFalse);
+      },
+    );
+
+    test(
+      "when s is '()', "
+      "then expect to return true",
+      () {
+        expect(solution.isValid("()"), isTrue);
+      },
+    );
+
+    test(
+      "when s is '()[]{}', "
+      "then expect to return true",
+      () {
+        expect(solution.isValid("()[]{}"), isTrue);
+      },
+    );
+
+    test(
+      "when s is '(]', "
+      "then expect to return false",
+      () {
+        expect(solution.isValid("(]"), isFalse);
+      },
+    );
+
+    test(
+      "when s is '([])', "
+      "then expect to return true",
+      () {
+        expect(solution.isValid("([])"), isTrue);
+      },
+    );
+
+    test(
+      "when s is '([)]', "
+      "then expect to return false",
+      () {
+        expect(solution.isValid("([)]"), isFalse);
+      },
+    );
+
+    test(
+      "when s is '([]{})]', "
+      "then expect to return false",
+      () {
+        expect(solution.isValid("([)]"), isFalse);
+      },
+    );
+  });
+}
