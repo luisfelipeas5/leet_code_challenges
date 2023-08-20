5143fffb98c201b80900437a6c618415c6f23dbb
luisfelipeas5 <luisfelipeas5@gmail.com>
2023-08-20 17:46:06 -0300

    feat: implements min and max after min solution (B solution)

--- test/max_profit/max_profit_test.dart
+++ test/max_profit/max_profit_test.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:leet_code_challenges/max_profit/max_profit.dart';
 import 'package:test/test.dart';
 
 void main() {
   late Solution solution;
 
-  group("Best Time to Buy and Sell Stock", () {
+  group("Best Time to Buy and Sell Stock /", () {
     setUp(() {
       solution = Solution();
     });
@@ -24,6 +27,54 @@ void main() {
       () {
         final prices = [7, 6, 4, 3, 1];
         expect(solution.maxProfit(prices), 0);
+      },
+    );
+
+    test(
+      "when prices = [2,1,2,0,1], "
+      "then expect to return 1",
+      () {
+        final prices = [2, 1, 2, 0, 1];
+        expect(solution.maxProfit(prices), 1);
+      },
+    );
+
+    test(
+      "when prices = [7,2,4,1], "
+      "then expect to return 2",
+      () {
+        final prices = [7, 2, 4, 1];
+        expect(solution.maxProfit(prices), 2);
+      },
+    );
+
+    test(
+      "when prices = [11,7,2,4,1], "
+      "then expect to return 2",
+      () {
+        final prices = [11, 7, 2, 4, 1];
+        expect(solution.maxProfit(prices), 2);
+      },
+    );
+
+    test(
+      "when prices = 'large price list', "
+      "then expect to return 3",
+      () {
+        final pricesString =
+            File('test/max_profit/large_prices.json').readAsStringSync();
+        final List<dynamic> dynamicPrices = jsonDecode(pricesString)["prices"];
+        final List<int> prices = dynamicPrices.cast<int>();
+        expect(solution.maxProfit(prices), 3);
+      },
+    );
+
+    test(
+      "when prices = [5,7,2,7,3,3,5,3,0], "
+      "then expect to return 5",
+      () {
+        final prices = [5, 7, 2, 7, 3, 3, 5, 3, 0];
+        expect(solution.maxProfit(prices), 5);
       },
     );
   });
